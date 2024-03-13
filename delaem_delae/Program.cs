d79d89d2ef8aa468e7ec7c494e645828faebc33c
Giorf81 <rejroberts7@gmail.com>
2024-03-13 21:29:30 +0300

    Добавьте файлы проекта.

---
+++ delaem_delae/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace delaem_delae
+{
+    class BaseClass
+    {
+        public virtual int Counter
+        {
+            get;
+            set;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        private int counter;
+        public override int Counter
+        {
+            get
+            {
+                return counter;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    counter = value;
+                }
+            }
+        }
+    }
+
+
+}
